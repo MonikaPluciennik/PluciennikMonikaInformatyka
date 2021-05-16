0773501f64e723d6588ee3114f23f32dd391b370
MonikaPluciennik <monfen1812@gmail.com>
2021-05-16 14:02:54 +0200

    Zadanie na zajeciach

--- Zad.3/srednia arytmetyczna l. np.py
+++ Zad.3/srednia arytmetyczna l. np.py
@@ -1,14 +1,11 @@
 
-a=int(input("podaj pierwszą liczbe z przedizału:"))
-b=int(input("podaj ostatnia liczbe z przedizału:"))
-suma=0
-n=0
-i=a
-while i<b:
-    while i%2==0:
-        i=i+1
-        suma=suma+i
-        n=n+1
-        i=i+2
-        srednia=suma/n
-print("srednia arytmetyczna l.np.:",srednia)
+lista = [2,5,1,4,9]
+
+def funkcjazargumentem(lista):
+    print(lista)
+
+n = len(lista)
+
+
+    
+
